0804c99acc8360f51f004fbbb5a3504c3b201505
unknown <andre.lb865854@gmail.com>
2025-10-18 13:07:44 -0300

    components

---
+++ 1_componentes/src/components/FunctionalComponent.jsx
@@ -0,0 +1,12 @@
+// Componente filho
+function FunctionalComponent() {
+
+    return (
+        <div>
+            <h2>Testando o componente</h2>
+        </div>
+    )
+
+}
+
+export default FunctionalComponent;
